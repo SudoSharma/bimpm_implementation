be033e7d82385aa0377c10bb016f1b784f08f03d
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-10-11 23:18:08 +0000

    [add] create outline for potential layers API

---
+++ model/layers.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import plac
+
+def main():
+    pass
+
+class WordRepresentationLayer(nn.Module):
+    def __init__(self):
+        pass
+
+
+class ContextRepresentationLayer(nn.Module):
+    def __init__(self):
+        pass
+
+
+class MatchingLayer(nn.Module):
+    def __init__(self):
+        pass
+
+
+class AggregationLayer(nn.Module):
+    def __init__(self):
+        pass
+
+
+class PredictionLayer(nn.Module):
+    def __init__(self):
+        pass
