41fddbd3726e3e6325c1a08a9129ddbacfb58686
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-10-12 00:53:23 +0000

    [add] finish rough bimpm model API

--- model/bimpm.py
+++ model/bimpm.py
@@ -10,20 +10,24 @@ def main():
 
 class BiMPM(nn.Module):
     def __init__(self):
+
         super(BiMPM, self).__init__()
 
         self.w_layer = layer.WordRepresentationLayer()
         self.c_layer = layer.ContextRepresentationLayer()
-        self.m_layer = MatchingLayer()
-        self.a_layer = AggregationLayer()
-        self.p_layer = PredictionLayer()
+        self.m_layer = layer.MatchingLayer()
+        self.a_layer = layer.AggregationLayer()
+        self.p_layer = layer.PredictionLayer()
 
 
-    def forward(self, data):
-        p, q = data
-        p_out, q_out = self.w_layer(p, q)
+    def forward(self, p, q):
 
-        pass
+        p, q = self.w_layer(p), self.w_layer(q)
+        p, q = self.c_layer(p), self.c_layer(q)
+        p, q = self.m_layer(p, q)
+        p, q = self.a_layer(p), self.a_layer(q)
+
+        return self.p_layer(p, q)
 
 
 if __name__() == "__main__":
