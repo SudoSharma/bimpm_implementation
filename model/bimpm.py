e8034547c31f5efb902efc421167d91954b5fe8d
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-10-12 19:59:08 +0000

    [fix] modify inputs to prediction layer

--- model/bimpm.py
+++ model/bimpm.py
@@ -9,24 +9,28 @@ def main():
 
 
 class BiMPM(nn.Module):
-    def __init__(self):
+    def __init__(self, args, data):
         super(BiMPM, self).__init__()
 
-        self.w_layer = layer.WordRepresentationLayer()
-        self.c_layer = layer.ContextRepresentationLayer()
-        self.m_layer = layer.MatchingLayer()
-        self.a_layer = layer.AggregationLayer()
-        self.p_layer = layer.PredictionLayer()
+        self.args = args
+        self.w_layer = layer.WordRepresentationLayer(args, data)
+        self.c_layer = layer.ContextRepresentationLayer(args)
+        self.m_layer = layer.MatchingLayer(args)
+        self.a_layer = layer.AggregationLayer(args)
+        self.p_layer = layer.PredictionLayer(args)
 
 
     def forward(self, p, q):
-
+        # TODO create sentence object
+        # 'p' should be a sentence object with two attributes,
+        # a chars attribute of shape (seq_len, max_word_len) 
+        # a words attribute of shape (batch_size, seq_len)
         p, q = self.w_layer(p), self.w_layer(q)
         p, q = self.c_layer(p), self.c_layer(q)
         p, q = self.m_layer(p, q)
-        p, q = self.a_layer(p), self.a_layer(q)
+        match_vec = self.a_layer(p), self.a_layer(q)
 
-        return self.p_layer(p, q)
+        return self.p_layer(match_vec)
 
 
 if __name__() == "__main__":
