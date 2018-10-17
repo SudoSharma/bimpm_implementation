e6c00f936e041c4ecb72026376ad5e7e509c51b4
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-10-17 15:49:05 +0000

    [add][fix] refactor code and create sentence class for data loading

--- model/utils.py
+++ model/utils.py
@@ -1,13 +1,40 @@
+import torch
+from torch.autograd import Variable
 from torchtext import data
 from torchtext import datasets
 from torchtext.vocab import GloVe
 from abc import ABC, abstractmethod
 
 
-class ModelData(ABC):
-    @abstractmethod
+class SNLI:
     def __init__(self, args):
-        pass
+        self.args = args
+
+        self.TEXT = data.Field(batch_first=True, tokenize='spacy')
+        self.LABEL = data.LabelField()
+
+        self.train, self.valid, self.test = datasets.SNLI.splits(
+            self.TEXT, self.LABEL)
+
+        self.TEXT.build_vocab(
+            self.train,
+            self.valid,
+            self.test,
+            vectors=GloVe(name='840B', dim=300))
+        self.LABEL.build_vocab(self.train)
+
+        self.train_iter, self.valid_iter = data.BucketIterator.splits(
+            (self.train, self.valid, self.test),
+            batch_sizes=[args.batch_size] * 3,
+            device=args.gpu)
+
+        self.max_word_len = max([len(w) for w in self.TEXT.vocab.itos])
+        self.char_vocab = {'': 0}
+        self.word_chars = [[0] * self.max_word_len, [0] * self.max_word_len]
+
+        self.build_char_vocab()
+
+        self.last_epoch = -1
 
     def build_char_vocab(self):
         for word in self.TEXT.vocab.itos[2:]:
@@ -23,69 +50,108 @@ class ModelData(ABC):
 
     def words_to_chars(self, batch):
         batch = batch.data.cpu().numpy().astype(int).tolist()
-        return = [[self.word_chars]]
+        return [[self.word_chars[w] for w in words] for words in batch]
 
+    def keep_training(self, iterator):
+        self.present_epoch = int(iterator.epoch)
+        if self.present_epoch == self.args.epoch:
+            return False
+        if self.present_epoch > self.last_epoch:
+            print(f'epoch: {self.present_epoch+1}')
+        self.last_epoch = self.present_epoch
+        return True
 
-class SNLI(ModelData):
+class Quora:
     def __init__(self, args):
-        self.TEXT = data.Field(batch_first=True, tokenize=)
-        self.LABEL = data.LabelField()
+        self.args = args
 
-        self.train, self.valid, self.test = data.SNLI.splits(
-                self.TEXT, self.LABEL)
-
-        self.TEXT.build_vocab(
-                self.train, self.valid, self.test,
-                vectors=GloVe(name='840B', dim=300))
-        self.LABEL.build_vocab(self.train)
-
-        self.train_iter, self.valid_iter = data.BucketIterator.splits(
-                (self.train, self.valid, self.test),
-                batch_sizes=[args.batch_size]*3,
-                device=args.gpu)
-
-        self.max_word_len = max([len(w) for w in self.TEXT.vocab.itos])
-        self.char_vocab = {'': 0}
-        self.word_chars = [[0] * self.max_word_len, [0] * self.max_word_len]
-
-        self.build_char_vocab()
-
-
-class Quora(ModelData):
-    def __init__(self, args):
         self.RAW = data.RawField()
         self.TEXT = data.Field(batch_first=True)
         self.LABEL = data.LabelField()
 
-        self.fields = [
-                ('label', self.LABEL),
-                ('q1', self.TEXT),
-                ('q2', self.TEXT),
-                ('q2', self.RAW)]
+        self.fields = [('label', self.LABEL), ('q1', self.TEXT),
+                       ('q2', self.TEXT), ('q2', self.RAW)]
 
         self.train, self.valid, self.test = data.TabularDataset.splits(
-                path='./data/quora',
-                train='train.tsv',
-                validation='dev.tsv',
-                test='test.tsv',
-                format='tsv',
-                fields=self.fields)
+            path='./data/quora',
+            train='train.tsv',
+            validation='dev.tsv',
+            test='test.tsv',
+            format='tsv',
+            fields=self.fields)
 
         self.TEXT.build_vocab(
-                self.train, self.valid, self.test,
-                vectors=GloVe(name='840B', dim=300))
+            self.train,
+            self.valid,
+            self.test,
+            vectors=GloVe(name='840B', dim=300))
         self.LABEL.build_vocab(self.train)
 
         self.sort_key = lambda x: data.interleave_keys(len(x.q1), len(x.q2))
 
         self.train_iter, self.valid_iter = data.BucketIterator.splits(
-                (self.train, self.valid, self.test),
-                batch_sizes=[args.batch_size]*3,
-                device=args.gpu,
-                sort_key=self.sort_key)
+            (self.train, self.valid, self.test),
+            batch_sizes=[args.batch_size] * 3,
+            device=args.gpu,
+            sort_key=self.sort_key)
 
         self.max_word_len = max([len(w) for w in self.TEXT.vocab.itos])
         self.char_vocab = {'': 0}
         self.word_chars = [[0] * self.max_word_len, [0] * self.max_word_len]
 
         self.build_char_vocab()
+
+        self.last_epoch = -1
+
+    def build_char_vocab(self):
+        for word in self.TEXT.vocab.itos[2:]:
+            chars = []
+            for c in list(word):
+                if c not in self.char_vocab:
+                    self.char_vocab[c] = len(self.char_vocab)
+
+                chars.append(self.char_vocab[c])
+
+            chars.extend([0] * (self.max_word_len - len(word)))
+            self.word_chars.append(chars)
+
+    def words_to_chars(self, batch):
+        batch = batch.data.cpu().numpy().astype(int).tolist()
+        return [[self.word_chars[w] for w in words] for words in batch]
+
+    def keep_training(self, iterator):
+        self.present_epoch = int(iterator.epoch)
+        if self.present_epoch == self.args.epoch:
+            return False
+        if self.present_epoch > self.last_epoch:
+            print(f'epoch: {self.present_epoch+1}')
+        self.last_epoch = self.present_epoch
+        return True
+
+
+class Sentence:
+    def __init__(self, batch, model_data, data_type):
+        self.batch, self.model_data = batch, model_data
+
+        if data_type == 'SNLI':
+            self.p, self.q = 'premise', 'hypothesis'
+        elif data_type == 'Quora':
+            self.p, self.q = 'q1', 'q2'
+
+    def process_batch(self):
+        self.p = getattr(self.batch, self.p)
+        self.q = getattr(self.batch, self.q)
+
+        self.char_p = Variable(
+            torch.LongTensor(self.model_data.words_to_char(self.p)))
+        self.char_q = Variable(
+            torch.LongTensor(self.model_data.words_to_char(self.q)))
+
+    def make_data_dict(self):
+        self.p = {'words': self.p, 'chars': self.char_p}
+        self.q = {'words': self.q, 'chars': self.char_q}
+
+    def generate(self):
+        self.process_batch
+        self.make_data_dict
+        return (self.p, self.q)
