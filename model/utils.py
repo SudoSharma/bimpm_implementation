ff41702f173c403792e3a8de6ab085da39426dae
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-10-22 05:27:21 +0000

    [add] finish docstrings for utils

--- model/utils.py
+++ model/utils.py
@@ -1,15 +1,39 @@
+"""Constructs data processors and loaders for Quora and SNLI sentences,
+as well a container object, Args, to hold all arguments passed during
+training and testing script execution to initialize the BiMPM model.
+
+"""
+
 import torch
 from torch.autograd import Variable
 from torchtext import data
 from torchtext import datasets
 from torchtext.vocab import GloVe
-from abc import ABC, abstractmethod
 
 
 class SNLI:
+    """A data processor for SNLI data, which splits the original dataset
+    into a training, validation, and test. Also providers iterators, and
+    methods to process words within sentences and characters within words.
+
+    This data can be fed into the WordRepresentationLayer for the BiMPM model.
+
+    """
+
     def __init__(self, args):
+        """Initialize the data loader, split data into train, valid, and
+        test sets, and create iterators. Also create word and char vocabulary
+        objects as part of the preprocessing pipeline.
+
+        Parameters
+        ----------
+        args : Args
+            An object with all arguments for BiMPM model.
+
+        """
         self.args = args
 
+        # Define how input data should be processed
         self.TEXT = data.Field(batch_first=True, tokenize='spacy')
         self.LABEL = data.LabelField()
 
@@ -29,18 +53,23 @@ class SNLI:
                 batch_sizes=[args.batch_size] * 3,
                 device=args.device)
 
-        self.train_iter.repeat = True
+        self.train_iter.repeat = True  # Allow default unlimited epochs
 
         self.max_word_len = max([len(w) for w in self.TEXT.vocab.itos])
+        # Handle <pad> and <unk>
         self.char_vocab = {'': 0}
         self.word_chars = [[0] * self.max_word_len, [0] * self.max_word_len]
 
         self.build_char_vocab()
 
-        self.last_epoch = -1
+        self.last_epoch = -1  # Allow atleast one epoch
 
     def build_char_vocab(self):
-        for word in self.TEXT.vocab.itos[2:]:
+        """Create char vocabulary, generate char2idx and idx2char mapping,
+        and pad words to max word length.
+
+        """
+        for word in self.TEXT.vocab.itos[2:]:  # Skip <pad> and <unk>
             chars = []
             for c in list(word):
                 if c not in self.char_vocab:
@@ -48,14 +77,46 @@ class SNLI:
 
                 chars.append(self.char_vocab[c])
 
+            # Pad words until max word length
             chars.extend([0] * (self.max_word_len - len(word)))
             self.word_chars.append(chars)
 
     def words_to_chars(self, batch):
+        """Convert batch of sentences to appropriately shaped array for
+        the WordRepresentationLayer. This will eventually be turned into
+        a Variable object to track gradients and allow for easy
+        backpropagation of errors later on.
+
+        Parameters
+        ----------
+        batch : Variable
+            A PyTorch Variable with shape (batch_size, seq_len).
+
+        Returns
+        -------
+        array_like
+            An nested array with shape (batch_size, seq_len, max_word_len).
+
+        """
         batch = batch.data.cpu().numpy().astype(int).tolist()
-        return [[self.word_chars[w] for w in words] for words in batch]
+        return [[self.word_chars[word] for word in sentence]
+                for sentence in batch]
 
     def keep_training(self, iterator):
+        """Track batch iteration and epochs.
+
+        Parameters
+        ----------
+        iterator : Iterator
+            An iterator object which provides batches of data, and keeps a
+            track of the epochs.
+
+        Returns
+        -------
+        bool
+            True if all epochs are complete, else False.
+
+        """
         self.present_epoch = int(iterator.epoch)
         if self.present_epoch == self.args.epoch:
             return False
@@ -66,11 +127,33 @@ class SNLI:
 
 
 class Quora:
+    """A data processor for Quora data, which splits the original dataset
+    into a training, validation, and test. Also providers iterators, and
+    methods to process words within sentences and characters within words.
+
+    This data can be fed into the WordRepresentationLayer for the BiMPM model.
+
+    """
+
     def __init__(self, args, toy=False):
+        """Initialize the data loader, split data into train, valid, and
+        test sets, and create iterators. Also create word and char vocabulary
+        objects as part of the preprocessing pipeline.
+
+        Parameters
+        ----------
+        args : Args
+            An object with all arguments for BiMPM model.
+        toy : bool, optional
+            Indicates whether smaller dataset should be used for testing and
+            experimentation.
+
+        """
         self.args = args
 
+        # Define how input data should be processed
         self.RAW = data.RawField()
-        self.RAW.is_target = False
+        self.RAW.is_target = False  # Fix for PyTorch handling of Raw Fields
         self.TEXT = data.Field(batch_first=True)
         self.LABEL = data.LabelField()
 
@@ -101,17 +184,22 @@ class Quora:
                 device=args.device,
                 sort_key=self.sort_key)
 
-        self.train_iter.repeat = True
+        self.train_iter.repeat = True  # Allow default unlimited epochs
 
         self.max_word_len = max([len(w) for w in self.TEXT.vocab.itos])
+        # Handle <pad> and <unk>
         self.char_vocab = {'': 0}
         self.word_chars = [[0] * self.max_word_len, [0] * self.max_word_len]
 
         self.build_char_vocab()
 
-        self.last_epoch = -1
+        self.last_epoch = -1  # Allow atleast one epoch
 
     def build_char_vocab(self):
+        """Create char vocabulary, generate char2idx and idx2char mapping,
+        and pad words to max word length.
+
+        """
         for word in self.TEXT.vocab.itos[2:]:
             chars = []
             for c in list(word):
@@ -124,10 +212,40 @@ class Quora:
             self.word_chars.append(chars)
 
     def words_to_chars(self, batch):
+        """Convert batch of sentences to appropriately shaped array for
+        the WordRepresentationLayer. This will eventually be turned into
+        a Variable object to track gradients and allow for easy
+        backpropagation of errors later on.
+
+        Parameters
+        ----------
+        batch : Variable
+            A PyTorch Variable with shape (batch_size, seq_len).
+
+        Returns
+        -------
+        array_like
+            An nested array with shape (batch_size, seq_len, max_word_len).
+
+        """
         batch = batch.data.cpu().numpy().astype(int).tolist()
         return [[self.word_chars[w] for w in words] for words in batch]
 
     def keep_training(self, iterator):
+        """Track batch iteration and epochs.
+
+        Parameters
+        ----------
+        iterator : Iterator
+            An iterator object which provides batches of data, and keeps a
+            track of the epochs.
+
+        Returns
+        -------
+        bool
+            True if all epochs are complete, else False.
+
+        """
         self.present_epoch = int(iterator.epoch)
         if self.present_epoch == self.args.epoch:
             return False
@@ -138,7 +256,24 @@ class Quora:
 
 
 class Sentence:
+    """Creates a Sentence object to hold the words and characters for each
+    sentence in a batch.
+
+    """
+
     def __init__(self, batch, model_data, data_type):
+        """Initialize a Sentence object for SNLI or Quora data.
+
+        Parameters
+        ----------
+        batch : Variable
+            A PyTorch Variable with shape (batch_size, seq_len).
+        model_data : {Quora, SNLI}
+            A data loading object which returns word vectors and sentences.
+        data_type : {'Quora', 'SNLI'}, optional
+            Choose either SNLI or Quora (default is 'quora').
+
+        """
         self.batch, self.model_data = batch, model_data
 
         if data_type.lower() == 'snli':
@@ -147,9 +282,19 @@ class Sentence:
             self.p, self.q = 'q1', 'q2'
 
     def process_batch(self, device):
+        """Retrieve either SNLI or Quora data from each batch by lable, and
+        construct words and chars.
+
+        Parameters
+        ----------
+        device : {'cuda:0', 'cpu'}
+            Indicates whether to store the char tensors in the cpu or gpu.
+
+        """
         self.p = getattr(self.batch, self.p)
         self.q = getattr(self.batch, self.q)
 
+        # Track gradients on char tensors
         self.char_p = Variable(
             torch.LongTensor(self.model_data.words_to_chars(self.p)))
         self.char_q = Variable(
@@ -158,17 +303,42 @@ class Sentence:
         self.char_p = self.char_p.to(device)
         self.char_q = self.char_q.to(device)
 
-    def make_data_dict(self):
+    def make_sentence_dict(self):
+        """Create a dictionary for words and chars in a sentence."""
         self.p = {'words': self.p, 'chars': self.char_p}
         self.q = {'words': self.q, 'chars': self.char_q}
 
     def generate(self, device):
+        """Generate a sentence dictionary with words and chars for each
+        sentence
+
+        Parameters
+        ----------
+        device : {'cuda:0', 'cpu'}
+            Indicates whether to store the char tensors in the cpu or gpu.
+
+        Returns
+        -------
+        tuple
+            A tuple of sentence objects.
+
+        """
         self.process_batch(device)
-        self.make_data_dict()
+        self.make_sentence_dict()
         return (self.p, self.q)
 
 
 class Args:
+    """Creates a mapping from dictionary to object."""
     def __init__(self, args_dict):
+        """Initialize and store args from dict into self attributes for easy
+        access during runtime.
+
+        Parameters
+        ---------
+        args_dict : dict
+            A dictionary of all arguments passed to the training or testing
+            script.
+        """
         for k, v in args_dict.items():
             setattr(self, k, v)
