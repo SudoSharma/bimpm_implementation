957a378e4e562b593cdabc87dd64c1fc2f4080c8
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-10-22 02:32:43 +0000

    [add] finish docstring for general BiMPM API

--- model/bimpm.py
+++ model/bimpm.py
@@ -1,9 +1,27 @@
+"""Creates a BiMPM model architecture based on PyTorch's nn.Module."""
+
 import torch.nn as nn
 import model.layers as L
 
 
 class BiMPM(nn.Module):
+    """A bilateral multi-perspective matching model used for a variety of
+    NLP tasks, including paraphrase identification, natural language
+    inference,and asnwer selection.
+
+    """
+
     def __init__(self, args, model_data):
+        """Initialize the BiMPM model architecture.
+
+        Parameters
+        ----------
+        args : Args
+            An object with all arguments for BiMPM model.
+        model_data : {Quora, SNLI}
+            A data loading object which returns word vectors and sentences.
+
+        """
         super(BiMPM, self).__init__()
 
         self.args = args
@@ -14,9 +32,18 @@ class BiMPM(nn.Module):
         self.p_layer = L.PredictionLayer(args)
 
     def forward(self, p, q):
-        p, q = self.w_layer(p), self.w_layer(q)
-        p, q = self.c_layer(p), self.c_layer(q)
-        p, q = self.m_layer(p, q)
-        match_vec = self.a_layer(p, q)
+        """Defines forward pass computations flowing from inputs to
+        outputs in the network.
 
-        return self.p_layer(match_vec)
+        Parameters
+        ----------
+        p, q : Sentence
+            A sentence object with chars and word batches.
+
+        """
+        p, q = self.w_layer(p), self.w_layer(q)  # Create word embeddings
+        p, q = self.c_layer(p), self.c_layer(q)  # Incorporate context
+        p, q = self.m_layer(p, q)  # Compare contexts
+        match_vec = self.a_layer(p, q)  # Aggregate context vectors
+
+        return self.p_layer(match_vec)  # Generate task-specific probabilities
