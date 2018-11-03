4fa5fb42b4276591a93e44143e0456824bb225bc
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-11-03 02:47:36 +0000

    [add] include first draft of script based experimentation

--- train.sh
+++ train.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # run train script, store stdout and stderr in output file and capture PID
-nohup python train.py > train.out 2>&1 &
-echo $! > train_pid.txt
+
+# default
+if [ ! "$1" ]
+    nohup python train.py > train.out 2>&1 &
+    echo $! > train_pid.txt
+elif [ "$1" ==  --00 ] ; then  # xp_0.0
+    nohup python train.py -s > ./research/output/xp_0.0_output.txt 2>&1 &
+    echo $! > ./research/output/xp_0.0_pid.txt
+fi
