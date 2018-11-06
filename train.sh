fdfbc9cb0fa2fdfa5d4abd8c95837cb75b9fe23c
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-11-06 21:56:13 +0000

    [fix] set up train script to shutdown after first experiment

--- train.sh
+++ train.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # run train script, store stdout and stderr in output file and capture PID
-
 if [ "$1" == 00 ]; then  # xp_0.0
-    nohup python train.py -t -e 0.0 > ./research/output/xp_0.0_output.txt 2>&1 &
+    nohup python train.py -s -e 0.0 > ./research/output/xp_0.0_output.txt 2>&1 &
     echo $! > ./research/output/xp_0.0_pid.txt
 else  # Default
     nohup python train.py > train.out 2>&1 &
