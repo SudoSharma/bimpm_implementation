f76c4c82c2f9c7653f6cead16c2dc6f3ecadfe45
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-11-08 22:59:38 +0000

    [add] setup baseline experiment with more epochs

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # run train script, store stdout and stderr in output file and capture PID
 if [ "$1" == xp ]; then  # Run current experiment
-    nohup python train.py -char-hidden-size 100 -s -e 1.1 > ./research/output/xp_1.1_output.txt 2>&1 &
-    echo $! > ./research/output/xp_1.1_pid.txt
+    nohup python train.py -epoch 15 -s -e 0.2 > ./research/output/xp_0.2_output.txt 2>&1 &
+    echo $! > ./research/output/xp_0.2_pid.txt
 else  # Default
     nohup python train.py > train.out 2>&1 &
     echo $! > train_pid.txt
