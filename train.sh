93c81b7c2036be1c779ad0684c0da95357d3dbf6
SudoSharma <18308855+SudoSharma@users.noreply.github.com>
2018-11-09 14:57:15 +0000

    [add] setup for any new experiments

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # run train script, store stdout and stderr in output file and capture PID
 if [ "$1" == xp ]; then  # Run current experiment
-    nohup python train.py -data-type snli -s -e 0.2 > ./research/output/xp_0.2_output.txt 2>&1 &
-    echo $! > ./research/output/xp_0.2_pid.txt
+    nohup python train.py -s -e 3.0 > ./research/output/xp_3.0_output.txt 2>&1 &
+    echo $! > ./research/output/xp_3.0_pid.txt
 else  # Default
     nohup python train.py -s > train.out 2>&1 &
     echo $! > train_pid.txt
